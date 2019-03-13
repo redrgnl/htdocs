17e3f14a315a77e4788e86972a1c0bce17190a92
Re <47809253+redrgnl@users.noreply.github.com>
2019-03-13 23:03:42 +0700

    CI3 Bootstrap

---
+++ CI.3.E41171452_P2/miku/application/controllers/admin/Overview.php
@@ -0,0 +1,17 @@
+<?php
+
+class Overview extends CI_Controller {
+    public function __construct()
+    {
+		parent::__construct();
+	}
+
+	public function index()
+	{
+        // load view admin/overview.php
+        $this->load->view('admin/overview');
+	}
+    public function desktop2(){
+        $this->load->view('admin/dashboard2');
+    }
+}
